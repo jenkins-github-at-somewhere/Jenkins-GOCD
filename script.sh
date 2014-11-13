e8cb8a2eacdb18ae401067e8991f8fba535531e1
Baskaran Deivasigamani <baskaran.md@gmail.com>
2014-11-13 13:46:05 -0800

    Change download file name.

--- script.sh
+++ script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-while read line; do export $line; done <jenkins.properties;curl -o downloadedFile ${JENKINS_URL}job/${PUBLISH_JOB_NAME}/${PUBLISH_JOB_ID}/artifact/${PUBLISH_ARTIFACT}
+while read line; do export $line; done <jenkins.properties;curl -o result.html ${JENKINS_URL}job/${PUBLISH_JOB_NAME}/${PUBLISH_JOB_ID}/artifact/${PUBLISH_ARTIFACT}
