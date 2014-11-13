c88097d8ca7ac8ce6d8180772a8af4de10175573
jenkins-github-at-somewhere <jenkins-github-at-somewhere@users.noreply.github.com>
2014-11-13 14:46:46 -0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-while read line; do export $line; done <${PUBLISH_JOB_NAME}.properties;curl -o result.html ${JENKINS_URL}job/${PUBLISH_JOB_NAME}/${PUBLISH_JOB_ID}/artifact/${PUBLISH_ARTIFACT}
+while read line; do export $line; done <${1}.properties;curl -o result.html ${JENKINS_URL}job/${PUBLISH_JOB_NAME}/${PUBLISH_JOB_ID}/artifact/${PUBLISH_ARTIFACT}
