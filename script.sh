f2b5c879525f6becfbca8d574d9603858153ac41
Baskaran Deivasigamani <baskaran.md@gmail.com>
2014-11-13 13:42:25 -0800

    Update script.sh with correct variables

--- script.sh
+++ script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-while read line; do export $line; done <jenkins.properties;curl -o downloadedFile ${JENKINS_URL}job/${JENKINS_JOB_NAME}/${JENKINS_JOB_NUMBER}/artifact/${JENKINS_ARTIFACT}
+while read line; do export $line; done <jenkins.properties;curl -o downloadedFile ${JENKINS_URL}job/${PUBLISH_JOB_NAME}/${PUBLISH_JOB_ID}/artifact/${PUBLISH_ARTIFACT}
