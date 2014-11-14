8a9a1b0ab8346abd45b00a34211d7f837b7596f5
Baskaran <baskaran.md@gmail.com>
2014-11-14 11:48:19 -0800

    Add MetaData Pusher script

---
+++ MetaDataPusher.sh
@@ -0,0 +1,118 @@
+#!/bin/bash
+
+NOW=`date +%s`
+
+##
+# Write METADATA Information of the build to MetaFile
+##
+function writeData() {
+  echo "Writing METADATA Information: "
+  _file=${1}
+  _link=${2}
+  touch ${_file}
+  if [[ -f ${_link} ]]; then unlink ${_link}; fi
+  ln -s ${_file} ${_link}
+
+  cat > ${_file} <<EOL
+PUBLISH_GIT_BRANCH=${PUBLISH_GIT_BRANCH}
+PUBLISH_GIT_COMMIT=${PUBLISH_GIT_COMMIT}
+PUBLISH_JOB_NAME=${PUBLISH_JOB_NAME}
+PUBLISH_JOB_ID=${PUBLISH_JOB_ID}
+PUBLISH_ARTIFACT=${PUBLISH_ARTIFACT}
+######################################
+JENKINS_URL=${JENKINS_URL}
+JENKINS_JOB_NUMBER=${BUILD_NUMBER}
+######################################
+EOL
+
+  curl ${JENKINS_URL}job/${PUBLISH_BINARY_JOB_NAME}/${PUBLISH_BINARY_JOB_ID}/artifact/build.info --silent >> ${_file}
+
+}
+
+##
+# Prepare the appropriate MetaFile in location based on JobName
+##
+function prepareMetaFile(){
+
+  if [[ ${PUBLISH_JOB_NAME} == *_Sanity ]];
+  then
+     _file=Sanity_${NOW}.properties
+      writeData ${_file} latest_sanity
+
+   elif [[ ${PUBLISH_JOB_NAME} == *_Regression* ]];
+   then
+     _file=Regression_${NOW}.properties
+      writeData ${_file} latest_regression
+
+  elif [[ ${PUBLISH_JOB_NAME} == *_Performance* ]];
+  then
+     _file=Performance_${NOW}.properties
+      writeData ${_file} latest_performance
+  else
+     echo "Unknown Job!!"
+  fi
+}
+
+##
+# Commit MetaFile(s) to GIT
+##
+function commitToMaster() {
+
+  cd ${WORKSPACE}
+  git add .
+  git commit -m "${JENKINS_URL}job/${PUBLISH_JOB_NAME}/${PUBLISH_JOB_ID} [${PUBLISH_GIT_BRANCH}::${PUBLISH_GIT_COMMIT}] Checkin by jenkins";
+  git push origin master
+}
+
+##
+# Main Function goes here!!
+##
+function main() {
+
+  cd ${WORKSPACE}
+  git checkout master
+
+  if [[ ${PUBLISH_GIT_BRANCH} == Release_* ]] || [[ ${PUBLISH_GIT_BRANCH} == OPDK_* ]];
+    then
+
+    if [[ -f latest ]]; then unlink latest; fi
+    ln -s ReleaseBuilds latest
+    cd ${WORKSPACE}/ReleaseBuilds
+
+    #Create Release Build Directory
+    mkdir ${PUBLISH_GIT_BRANCH}
+
+    if [[ -f latest ]]; then unlink latest; fi
+    ln -s ${PUBLISH_GIT_BRANCH} latest
+
+    cd ${PUBLISH_GIT_BRANCH}
+    prepareMetaFile
+
+  elif [[ ${PUBLISH_GIT_BRANCH} == "master" ]];
+  then
+
+    if [[ -f latest ]]; then unlink latest; fi
+    ln -s MasterBuilds latest
+    cd ${WORKSPACE}/MasterBuilds
+
+    prepareMetaFile
+
+  else
+
+    if [[ -f latest ]]; then unlink latest; fi
+    ln -s FeatureBuilds latest
+    #Create Feature Build Directory
+    cd ${WORKSPACE}/FeatureBuilds
+
+    mkdir ${PUBLISH_GIT_BRANCH}
+    if [[ -f latest ]]; then unlink latest; fi
+    ln -s ${PUBLISH_GIT_BRANCH} latest
+
+    cd ${PUBLISH_GIT_BRANCH}
+    prepareMetaFile
+  fi
+
+  commitToMaster
+}
+
+main
